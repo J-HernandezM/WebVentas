d853e5af3a47d3f4fd7623af1aebcaf9a1dfc7c9
Juan <juhernandezm@unal.edu.co>
2022-11-23 19:36:24 -0500

    Se añade el mobile menu y el carrito, se evita colisiones entre ellos

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-const menuEmailDesk= document.querySelector(".userMenu");
 const userEmail= document.querySelector(".navBar__email");
+const mobileBurguer= document.querySelector(".menuHamIcon");
+const cartIcon= document.querySelector(".navBar__cart");
 
-userEmail.addEventListener("click", toggleElement);
+const menuEmailDesk= document.querySelector(".userMenu");
+const menuHamMobile= document.querySelector(".mobileMenu");
+const asideCart= document.querySelector(".shoppingCart");
 
-function toggleElement() {
-    menuEmailDesk.classList.toggle("inactive");
+
+
+userEmail.addEventListener("click", function(){toggleElement(menuEmailDesk)});
+mobileBurguer.addEventListener("click", function(){
+    const isAsideCartClosed=asideCart.classList.contains("inactive")
+        if(isAsideCartClosed){
+            toggleElement(menuHamMobile)
+        }
+        else{
+            asideCart.classList.add("inactive")
+            toggleElement(menuHamMobile)
+        }
+    });
+cartIcon.addEventListener("click", function(){
+    const isMenuMobileClosed=menuHamMobile.classList.contains("inactive");
+        if(isMenuMobileClosed){
+            toggleElement(asideCart);
+        }
+        else {
+            menuHamMobile.classList.add("inactive");
+            toggleElement(asideCart);
+        }
+    })
+
+
+function toggleElement(element) {
+    element.classList.toggle("inactive");
 }
 
